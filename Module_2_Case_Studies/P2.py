e5d796fd16cfb273104aec50dd592073b6d8a9ea
GyaneshMahto <61919561+Gyanesh-Mahto@users.noreply.github.com>
2020-03-13 11:14:40 +0530

    Delete P2.py

--- Module_2_Case_Studies/P2.py
+++
@@ -1,8 +0,0 @@
-'''
-2.What will be the output?
-d ={"john":40, "peter":45}print(list(d.keys()))
-Hint:d.keys()is the function which will show keys
-'''
-d ={"john":40, "peter":45}
-print(list(d.keys()))
-#['john', 'peter']
