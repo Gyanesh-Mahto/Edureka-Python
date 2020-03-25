0dd6115f00bb2acbabd85ffb5ef56b58ab4a90be
Gyanesh Mahto <61919561+Gyanesh-Mahto@users.noreply.github.com>
2020-03-25 19:28:05 +0530

    Update P12_modules.py

--- Class_Codes_Module_3&4/P12_modules.py
+++ Class_Codes_Module_3&4/P12_modules.py
@@ -33,5 +33,80 @@ print(dir(math))
 
 #from import statement:
 '''
-
+From import statement allows you to import specific attribute from a specific module into the
+current namespace.
 '''
+#example:
+from math import sqrt
+print(sqrt(4))  #2.0
+'''
+This statement does not import the entire module, it just imports required files in the
+module.
+'''
+
+#From import * statement
+'''
+The from...import * allows you to import all the attributes from the required module.
+This provides an easy way to import all the items from a module into the current namespaces.
+'''
+#from math import *
+
+#The Reload Function:
+'''
+When the module is imported into a script, the code in the top-level portion of a module is
+executed only once. Therefore, if you want to re-execute the top-level code in a module, 
+you can use the reload function. The reload function imports a previously imported module again.
+'''
+
+##Important Modules used in python:
+'''
+Modules:
+-> sys
+-> os
+-> math
+-> datetime
+-> random
+-> json
+'''
+
+#sys module:
+#-----------#
+'''
+-> This module provides access to some variables used or maintained by the interpreter and to
+functions that interact strongly with the interpreter. It is always available because when we
+install python, then this module is installed.
+'''
+import sys
+print(sys.argv)    #['P12_modules.py'] (stores any command line argument passed)
+#print(sys.exit())   #It informs the python interpreter to quit.
+
+print(not sys)
+#Returns False if module is imported, else True
+print(sys.flags)
+#Returns the struct sequence flags and exposes the status of command line flags
+print(sys.prefix)
+#Returns a string giving the site-specific directory prefix where the platform independent python
+#files are installed
+'''
+O/P:
+False
+sys.flags(debug=0, inspect=0, interactive=0, optimize=0, dont_write_bytecode=0, no_user_site=0, no_site=0, ignore_environment=0, verbose=0, bytes_warning=0, quiet=0, hash_randomization=1, isolated=0, dev_mode=False, utf8_mode=0)
+/home/mga/anaconda3
+'''
+
+##os module:
+#-----------#
+'''
+os module:
+-----------
+-> The os module includes code that lets Python work with your operating system and run some
+operating system commands.
+'''
+import os
+print(os.getcwd())
+
+print(os.chdir("~/Desktop/Edureka"))
+
+print(os.mkdir("test"))
+
+print(os.rmdir("~/Desktop/Edureka/test"))
